a90288759eccd222e35060f011b2acc5a4868d8b
Erik Barajas <erik.barajas93@gmail.com>
2016-12-29 12:07:46 -0500

    Uso restframework -cap.final --OSX

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 from django.conf.urls import url
-from apps.usuario.views import RegistroUsuario
+from apps.usuario.views import RegistroUsuario, UserAPI
 
 urlpatterns = [
-    url(r'^registrar$', RegistroUsuario.as_view(), name="registrar")
+    url(r'^registrar$', RegistroUsuario.as_view(), name="registrar"),
+    url(r'^api$', UserAPI.as_view(), name="api"),
 ]
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,12 +1,18 @@
 from __future__ import absolute_import
 
+import json
+from rest_framework.views import APIView
+
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponse
+
 
 from apps.usuario.forms import RegistroForm
+from apps.usuario.serializers import UserSerializer
 
 # Esta clase es la Default para las Formas de User.
 '''class RegistroUsuario(CreateView):
@@ -21,3 +27,12 @@ class RegistroUsuario(CreateView):
     template_name = "usuario/registrar.html"
     form_class    = RegistroForm
     success_url   = reverse_lazy("mascota:mascota_listar")
+
+class UserAPI(APIView):
+    serializer = UserSerializer
+
+    def get(self, request, format=None):
+        lista = User.objects.all()
+        response = self.serializer(lista, many=True)
+
+        return HttpResponse(json.dumps(response.data), content_type='application/json')
